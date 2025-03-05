9e824b8233c3c20c76e9fe972e216f7f059dbcac
liuzhi1993 <64524886+liuzhi1993@users.noreply.github.com>
2025-03-06 02:12:53 +0800

    Update main_C.R

--- main_C.R
+++ main_C.R
@@ -14,7 +14,8 @@ test_functions = function(x,i=0,sd=0.1,b=0.15){
     res = 1 + x - 0.45*exp(-50*(x-0.5)^2)
   }
   if(i==4){
-    res = -1.5*sd*x
+    #res = -1.5*sd*x
+    res = -1.5*0.2*x
   }
   if(i==5){
     res = -0.2*exp(-50*(x-0.5)^2)
@@ -58,21 +59,23 @@ library(tictoc)
 alpha = 0.05
 M = 100
 sd = 0.3
-n = c(400,800,1200,1600,2000)
-cv = c(3.07508,3.501234) # critical values for n = 400 and 800
+n = c(400,800,1200,1600,2000,2400,2800,3200)
+cv = c(3.07508,3.501234,3.279791,3.293203,3.222466,3.402945,3.490897,2.994733) 
+# critical values for n = 400, 800, 1200, 1600, 2000, 2400, 2800 and 3200.
 R = 100
 kernel = "epanechnikov"
 k = 0
-
-for (m in 1:3) {
+for (m in 1:8) {
   x = runif(n[m])
   x = seq(1/n[m],1,1/n[m])
-  tic()
-  c = simulate_c(n = n[m],X = x, R = R, kernel = kernel, k=k, alpha = alpha)
-  print(c(n[m],c))
-  toc()
+  #tic()
+  #c = simulate_c(n = n[m],X = x, R = 20, kernel = kernel, k=k, alpha = alpha)
+  #print(c(n[m],c))
+  #toc()
   c = cv[m]
-  for (i in c(3,7,4,15,0)) {
+  for (i in c(0,3,5,4,9)) {
+    # Test functions 0, 3, 5, 4, 9 correspond to f0,f1,f2,f3 and f4 in our paper,
+    # respectively
     x = runif(n[m])
     signal = test_functions(x,i,sd)
     res = 0 
@@ -81,7 +84,7 @@ for (m in 1:3) {
                                                        kernel="epanechnikov",sd = sd, k = k,c = c)},
                         times = M, unit = "s")
     toc()
-    #saveRDS(tm,paste("Time of F",i," with size ",n[m]))
+    saveRDS(tm,paste("Time of F",i," with size ",n[m]))
     print(paste("Rejection of F",i," with size ",n[m], " = ", res))
   }
 }
